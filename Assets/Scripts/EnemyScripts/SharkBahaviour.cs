f747a033be4ad48a9c2af0d30deb22f4cc3ecbb6
WillBoudreau <williamboudreau804@gmail.com>
2024-09-21 16:34:33 -0400

    Basic Shark ship and spawning implemented

--- Assets/Scripts/EnemyScripts/SharkBahaviour.cs
+++ Assets/Scripts/EnemyScripts/SharkBahaviour.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SharkBahaviour : MonoBehaviour
 {
+    public float stoppingDistance;
+    public float detectionDistance;
+    public Transform player;
+
+    public float speed;
+    public float damage;
+    public float health;
+    public float maxHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 2;
+        health = 100;
+        maxHealth = 100;
+        damage = 10;
+        stoppingDistance = 2;
+        detectionDistance = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player = GameObject.Find("Player").transform;
+        Move();
     }
+    public void Move()
+    {
+        if(Vector2.Distance(transform.position, player.position) < detectionDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,10 +9,18 @@ public class GameManager : MonoBehaviour
 
     public LevelManager levelManager;
     public GameObject player;
+    public GameObject Shark;
+    public GameObject SharkSpawn;
+    public GameObject[] enemySpawn;
+
+    public int NumSharks;
+
+    public List<Transform> spawnPoints = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
+        NumSharks = 5;
         uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
         uiManager.currentGameState = UIManager.GameState.MainMenu;
     }
@@ -20,7 +28,26 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        enemySpawn = GameObject.FindGameObjectsWithTag("EnemySpawn");
+        foreach (GameObject enemspawn in enemySpawn)
+        {
+            spawnPoints.Add(enemspawn.transform);
+        }
         uiManager.UpdateUI();
         player.SetActive(uiManager.currentGameState == UIManager.GameState.GamePlay);
+        if (levelManager.levelName == "GameTestScene")
+        {
+            spawnEnemy();
+        }
+    }
+    void spawnEnemy()
+    {
+        for(int i = 0; i < NumSharks; i++)
+        {
+            foreach (GameObject enemspawn in enemySpawn)
+            {
+                Instantiate(Shark, enemspawn.transform.position, Quaternion.identity);
+            }
+        }
     }
 }
