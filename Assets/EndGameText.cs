2560e9e39c3d21bed4c1d5380a4d88f28c78571a
WillBoudreau <williamboudreau804@gmail.com>
2024-11-29 16:11:07 -0500

    Game Over type effect added

--- Assets/EndGameText.cs
+++ Assets/EndGameText.cs
@@ -6,12 +6,26 @@ using UnityEngine.UI;
 
 public class EndGameText : MonoBehaviour
 {
+    [SerializeField] private UIManager uiManager;
     public TextMeshProUGUI text;
     public string GameOverText;
     public float speed = 0.1f;
+    bool hasShown = false;
+    void Update()
+    {
+        if (uiManager.currentGameState == UIManager.GameState.GameOver && !hasShown)
+        {
+            ShowText();
+        }
+        else if(uiManager.currentGameState != UIManager.GameState.GameOver && hasShown)
+        {
+            hasShown = false;
+        }
+    }
     public void ShowText()
     {
         StartCoroutine(ShowGameOverText());
+        hasShown = true;
     }
 
     public IEnumerator ShowGameOverText()
